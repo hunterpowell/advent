87f2e69a6b4beaf48b4c2cb4f50c873dc6ddae08
hunterpowell <161683842+hunterpowell@users.noreply.github.com>
2025-12-03 23:25:01 -0800

    cleaned up codebase

--- main.py
+++
@@ -1,8 +0,0 @@
-from code.day_1 import print_day_1
-from code.day_2 import day_2_part_1, day_2_part_2
-
-if __name__ == "__main__":
-    print_day_1()
-    print("Day 2 part 1: ", day_2_part_1())
-    print("Day 2 part 2: ", day_2_part_2())
-
