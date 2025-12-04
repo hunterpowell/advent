86f18cbd092d26bf9e7a63d910a43608a3cd415a
hunterpowell <161683842+hunterpowell@users.noreply.github.com>
2025-12-03 23:23:23 -0800

    day2pt2

--- 2025/code/day2.py
+++ 2025/code/day2.py
@@ -1,11 +1,5 @@
 
-def day2():
-    text = open("2025/data/day2.txt").read()
-
-    text = text.split(",")
-
-    tmp = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
-    tmp = tmp.split(",")
+def day2pt1(text):
 
     pt1 = 0
 
@@ -22,10 +16,11 @@ def day2():
             # only check for even length nums, odd ones auto fail
             if length % 2 == 0:
                 if num[:length//2] == num[length//2:]:
-                    total += x
+                    pt1 += x
 
-    # return pt1
+    return pt1
 
+def day2pt2(text):
     pt2 = set()
 
     for nums in text:
@@ -34,7 +29,7 @@ def day2():
         lower = int(nums_range[0])
         upper = int(nums_range[1])
 
-        print(f"lims: {lower}, {upper}")
+        # print(f"lims: {lower}, {upper}")
 
         for x in range(lower, upper+1):
             
@@ -55,9 +50,17 @@ def day2():
                 if number[:i] * mult == number:
                     # set to avoid duplicates
                     pt2.add(int(number))
-                    print(f"added: {number}")
+                    # print(f"added: {number}")
     # sum all invalid ids
     return sum(pt2)
 
+if __name__ == "__main__":
+    text = open("2025/data/day2.txt").read()
 
-print(day2())
+    text = text.split(",")
+
+    tmp = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    tmp = tmp.split(",")
+
+    print(f"1: {day2pt1(text)}")
+    print(f"2: {day2pt2(text)}")
